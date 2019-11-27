f5321546ac6bb7b1c548a3d1e31d74c09669f3b0
szymszp563 <szymon.szpakowski6@gmail.com>
2019-11-27 14:14:07 +0100

    patient pages translated to polish

--- src/main/java/pl/polsl/orderadoctor/controllers/IndexController.java
+++ src/main/java/pl/polsl/orderadoctor/controllers/IndexController.java
@@ -1,16 +1,23 @@
 package pl.polsl.orderadoctor.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
 public class IndexController {
 
     @GetMapping({"", "/", "/index"})
-    public String getIndexPage(Model model) {
+    public String getIndexPage(Model model, HttpServletRequest request, HttpServletResponse response) {
+
+        // token can be revoked here if needed
+        new SecurityContextLogoutHandler().logout(request, null, null);
 
         log.debug("In Index Controller");
 
